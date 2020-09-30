5dcf7697b7073e08fddcd2cde441ce0d14053924
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-09-30 22:37:05 +0530

    donne

--- jarvis.py
+++ jarvis.py
@@ -1,21 +1,29 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import wikipedia
+import smtplib
+import webbrowser as wb
 
 engine = pyttsx3.init()
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def time():
     Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is ")
     speak(Time)
+
 
 def date():
     year = int(datetime.datetime.now().year)
     month = int(datetime.datetime.now().month)
     day = int(datetime.datetime.now().day)
+    speak("the current date is")
     speak(day)
     speak(month)
     speak(year)
@@ -23,20 +31,27 @@ def date():
 
 def wishme():
     speak("Welcome back sir")
-    speak("the current time is ")
     time()
-    speak("the current date is")
     date()
     hour = datetime.datetime.hour
-    if hour >= 6 and hour<12:
+    if 6 <= hour < 12:
         speak("Good morning sir")
-    elif hour >= 12 and hour<18:
+    elif 12 <= hour < 18:
         speak("Good afternoon sir")
-    elif hour >= 18 and hour<24:
+    elif 18 <= hour < 24:
         speak("Good evening sir")
     else:
         speak("good night sir")
     speak("jarvis at your service sir Please tell me how can i help you")
+
+def sendEmail(to,content):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login('abc@gmail.com', '12345')
+    server.sendmail('abc@gmail.com', to, content)
+    server.close()
+
 
 def takeCommand():
     r = sr.Recognizer()
@@ -57,5 +72,36 @@ def takeCommand():
             return "None"
         return query
 
-takeCommand()
 
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query = takeCommand().lower()
+
+        if 'time' in query:
+            time()
+        elif 'date' in query:
+            date()
+        elif 'wikipedia' in query:
+            speak("Searching....")
+            query = query.replace("wikipedia", "")
+            result = wikipedia.summary(query, sentences=2)
+            print(result)
+            speak(result)
+        elif 'send email' in query:
+            try:
+                speak("What should i say")
+                content = takeCommand()
+                to = 'xyz@gmail.com'
+                sendEmail(to,content)
+                speak("Email has been sent")
+            except Exception as e:
+                print(e)
+                speak("Unable to send email")
+        elif 'search in chrome' in query:
+            speak("What should is search")
+            chromepath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            search = takeCommand().lower()
+            wb.get(chromepath).open_new_tab(search+'.com')
+        elif 'offline' in query:
+            quit()
